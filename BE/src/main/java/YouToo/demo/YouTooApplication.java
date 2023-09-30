0e9155c91e8a2fd6fb9cc09e85e1140ce87f3be1
rlaghdtlr012 <rlaghdtlr012@naver.com>
2023-09-30 18:54:34 +0900

    랜딩페이지

---
+++ BE/src/main/java/YouToo/demo/YouTooApplication.java
@@ -0,0 +1,13 @@
+package YouToo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTooApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/YouToo/demo/YouTooApplicationTests.java
@@ -0,0 +1,13 @@
+package YouToo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
