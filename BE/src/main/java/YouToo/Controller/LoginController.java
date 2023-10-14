2488fe66ca396df1f9a03b0cf771432127d3235f
rlaghdtlr012 <rlaghdtlr012@naver.com>
2023-10-14 19:09:10 +0900

    [FEAT] CORS 해결을 위한 config 설정

--- BE/src/main/java/YouToo/Controller/LoginController.java
+++ BE/src/main/java/YouToo/Controller/LoginController.java
@@ -1,11 +1,12 @@
 package YouToo.Controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(originPatterns = "http://localhost:8080")
 @Slf4j
 public class LoginController {
 
---
+++ BE/src/main/java/YouToo/config/WebConfig.java
@@ -0,0 +1,20 @@
+package YouToo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 모든 경로에 대해 모든 HTTP 메서드에 대한 CORS를 허용
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
