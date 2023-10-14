5fa7e29353494bf0243e5e51ba42e90413ad2b24
rlaghdtlr012 <rlaghdtlr012@naver.com>
2023-10-14 18:54:48 +0900

    [FEAT] local-login 테스트 응답 세팅

--- BE/src/main/java/YouToo/Controller/LoginController.java
+++ BE/src/main/java/YouToo/Controller/LoginController.java
@@ -1,2 +1,17 @@
-package YouToo.Controller;public class LoginController {
+package YouToo.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LoginController {
+
+    @PostMapping("/local-login")
+    public String localLogin() {
+        log.info("localLogin Success");
+        return "hello";
+    }
 }
--- BE/src/main/java/YouToo/YouTooApplication.java
+++ BE/src/main/java/YouToo/YouTooApplication.java
@@ -1,4 +1,4 @@
-package YouToo.demo;
+package YouToo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
